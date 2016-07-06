3f77ff6788828fedc88110a3c6767bc8c347bb32
WME-EH <william.millington-evans@english-heritage.org.uk>
2016-07-06 15:31:29 +0100

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ gulp.task('watch', ['clean'], function(done){
 
 gulp.task('build', ['clean'], function(done){
   runSequence(
-    ['sass', 'html', 'fonts', 'scripts'],
+    ['sass', 'html', 'fonts', 'scripts', 'copyImg'],
     function(){
       buildBrowserify({
         minify: isRelease,
